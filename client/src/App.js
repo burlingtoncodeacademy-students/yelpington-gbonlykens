e5d97d8e8ffefc6225d1185cfcd2e293f4a5b4b8
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-11-09 06:59:08 -0500

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
+// App.js is the Landing Page for Front-End Portion
 import './App.css';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Homepage 
+
+
 
 function App() {
   return (
---
+++ client/src/components/restaurants/Homepage.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import Map from './Map.js';
+import { Link } from 'react-router-dom';
+
+function Homepage () {
+    return(
+        
+    );
+}
+
+
