e0ab12143aaf9be2d5ad0dd8627d816b5ee2af69
Ghislaine Lykens <glykens@Ghislaines-MacBook-Pro.local>
2021-12-15 08:25:26 -0500

    Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 // React imports
-import React from 'react';
-import { useState } from 'react';
-import {Route, Routes, Navigate} from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // import styling
-import './App.css';
+import "./App.css";
 
 // component imports
 import Home from "./components/Home.js";
 import Restaurants from "./components/RestInfo.js";
-import Map from './components/Map';
-
+import Map from "./components/Map";
+import Atlantikos from "./components/restaurants/Atlantikos";
 
 function App(props) {
   // setting the state for map
@@ -20,44 +20,18 @@ function App(props) {
     center: [25.76232, -80.19582],
   });
 
- return (
-   <div id = "page-container"> 
-    <h1 id = "page-title">
-     <a href = "/">
-     <span>Yelpington</span>
-     </a>
-     </h1>
-     <div id = "routes">
-       <div id = "map-border">
-         <Map
-         newZoom={zoom.zoomIn ? zoom.zoom : 15}
-         newCenter={zoom.zoomIn ? zoom.center : [25.76232, -80.19582]}
-         />
-       </div>
-       <Routes>
-         <Route exact path="/">
-           <Home setZoom={setZoom} />
-         </Route>
-         <Route
-          path = "/restaurant/:id"
-          children = {(props) => {
-            return props.match.isExact ? (
-              <Restaurants
-              match={props.match}
-              setZoom={setZoom}
-              zoomIn={zoom.zoomIn}
-              />
-            ) : (
-          
-              <Navigate to = "/" />
-            );
-          }}
-          />
-       </Routes>
-     </div>
-    </div>
+  return( 
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/restaurants/atlantikos" element={<Atlantikos />} />
+
+    </Routes>
+    </BrowserRouter>
+
+    </>
   );
- 
 }
 
 export default App;
